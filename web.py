14a34184ee45d24d8e1683f0397e9539389a8d3e
Tanner Winkelman <winkelmantanner@gmail.com>
2018-10-14 05:42:36 -0500

    Shows "agreement score".

--- web.py
+++ web.py
@@ -16,6 +16,45 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 # This file create each individual webpage for our the projec by rendering the HTML that we 
 # wrote in order to output the information we want
 
+
+def generate_index_pairs(size):
+    for outer_index in range(size):
+        for inner_index in range(size):
+            if outer_index != inner_index:
+                yield outer_index, inner_index
+
+def evaluate_articles_against_each_other(all_articles):
+    with open(pred.file_test_instances, 'w') as stance_file, open(pred.file_test_bodies, 'w') as body_file:
+        body_file.write('Body ID,articleBody\n')
+        for index, content_article in enumerate(all_articles['articles']):
+            body_file.write(str(index) + ",\"" + str(content_article['content']).replace('\"','') + '\"\n')
+        stance_file.write('Headline,Body ID\n')
+        for stance_article_index, content_article_index in generate_index_pairs(len(all_articles['articles'])):
+            stance_file.write('\"' + str(all_articles['articles'][stance_article_index]['title']).replace('\"','') + "\"," + str(content_article_index) + '\n')
+    result = pred.run()
+    point_count = [0 for k in range(len(all_articles['articles']))]
+    appearance_count = [0 for k in range(len(all_articles['articles']))]
+    index = 0
+    for stance_article_index, content_article_index in generate_index_pairs(len(all_articles['articles'])):
+        verdict = result[index]
+        appearance_count[stance_article_index] += 1
+        appearance_count[content_article_index] += 1
+        if verdict == 0: # if they agree
+            point_count[stance_article_index] += 1
+            point_count[content_article_index] += 1
+        elif verdict == 1: # disagree
+            point_count[stance_article_index] -= 1
+            point_count[content_article_index] -= 1
+        index += 1
+    scores = [point_count[i] / appearance_count[i] for i in range(len(all_articles['articles']))]
+    print(scores)
+    for index, article in enumerate(all_articles['articles']):
+        article['agreement_score'] = scores[index]
+
+
+
+
+
 @app.route('/')
 def home():
     # Init
@@ -29,13 +68,16 @@ def home():
     all_articles = newsapi.get_everything(q='America',
     language='en')
 
+    # print("NUM ARTICLES: " + str(len(all_articles['articles'])))
+
     # /v2/sources
     sources = newsapi.get_sources()
 
-    pred.run()
+    evaluate_articles_against_each_other(all_articles)
+
     
     #print(sources)
-    print(type(all_articles))
+    # print(type(all_articles))
     #print(top_headlines)
     #print(newsapi)
     
@@ -68,7 +110,11 @@ def display_exam():
     all_articles = newsapi.get_everything(q=str(Subject),
     language='en')
 
+    evaluate_articles_against_each_other(all_articles)
 
+
+    # for a in all_articles['articles']:
+    #     print(len( a['content']))
 
     # /v2/sources
     sources = newsapi.get_sources()
